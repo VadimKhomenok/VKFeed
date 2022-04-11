15eccdda10a8378bafea08f61d28f2d4d4b8e0ea
Vadim Khomenok <v.khomenok@gmail.com>
2022-04-11 17:02:24 +0200

    [TESTS] RemoteFeedLoader initialization tests

--- VKFeedTests/RemoteFeedLoaderTests.swift
+++ VKFeedTests/RemoteFeedLoaderTests.swift
@@ -7,26 +7,27 @@
 
 import XCTest
 
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
 class RemoteFeedLoaderTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        let _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_init_requestDataFromURL() {
+        let client = HTTPClient()
+        client.requestedURL = URL(string: "https://api-url.com")
+        let _ = RemoteFeedLoader()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
