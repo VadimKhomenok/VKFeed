95d9386789632f74026c9c1ef3351cc88b65f6bb
Vadim Khomenok <>
2022-04-11 15:46:11 +0200

    [DEV] Project created

---
+++ VKFeed/VKFeed.h
@@ -0,0 +1,18 @@
+//
+//  VKFeed.h
+//  VKFeed
+//
+//  Created by Vadim Khomenok on 11.04.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VKFeed.
+FOUNDATION_EXPORT double VKFeedVersionNumber;
+
+//! Project version string for VKFeed.
+FOUNDATION_EXPORT const unsigned char VKFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKFeed/PublicHeader.h>
+
+
