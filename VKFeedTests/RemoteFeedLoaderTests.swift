7d9545e95519ef31a1718256a618c41860966157
Vadim Khomenok <v.khomenok@gmail.com>
2022-04-12 20:52:24 +0200

    [TESTS] Added test to check case with 200 status code and empty array

--- VKFeedTests/RemoteFeedLoaderTests.swift
+++ VKFeedTests/RemoteFeedLoaderTests.swift
@@ -63,6 +63,18 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversNoItemsOn200StatusCodeWithEmptyJson() {
+        let (sut, client) = makeSUT()
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
+        
+        let emptyJson = Data("{\"items\":[]}".utf8)
+        client.complete(withStatusCode: 200, data: emptyJson)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://default-api-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
