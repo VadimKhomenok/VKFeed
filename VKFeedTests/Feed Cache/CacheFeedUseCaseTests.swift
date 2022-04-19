fb701b275af49c2c51bccb84ddeb6508e8c570db
Vadim Khomenok <v.khomenok@gmail.com>
2022-04-19 17:47:00 +0200

    [TESTS] Implemented test to check that calling save(items:) method of CacheFeedLoader deletes old cache

--- VKFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ VKFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -22,6 +22,10 @@ class CacheFeedLoader {
     init(store: FeedStore = FeedStore()) {
         self.store = store
     }
+    
+    func save(items: [FeedItem]) {
+        store.deleteCache()
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -30,5 +34,29 @@ class CacheFeedUseCaseTests: XCTestCase {
         let _ = CacheFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCallsCount, 0)
+    }
+    
+    func test_saveCache_triggersDeleteOnStore() {
+        let store = FeedStore()
+        let sut = CacheFeedLoader(store: store)
+        
+        let items = [makeUniqueFeedItem(), makeUniqueFeedItem()]
+        sut.save(items: items)
+        
+        XCTAssertEqual(store.deleteCallsCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    func makeUniqueFeedItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: nil, location: nil, imageUrl: anyURL())
+    }
+    
+    func anyURL() -> URL {
+        return URL(string: "https://api-url.com")!
+    }
+    
+    func anyNSError() -> NSError {
+        return NSError(domain: "An error", code: 400)
     }
 }
