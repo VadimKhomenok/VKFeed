b23e27c77667864f13c270270f5f1c1612cbbd82
Vadim Khomenok <v.khomenok@gmail.com>
2022-04-21 18:10:58 +0200

    Implemented the insertion of data when the cache is empty and retrieval of data if the cache is non-empty cases; delete cache artifacts from the device before and after each test

--- VKFeedTests/Feed Cache/CodableFeedStoreUseCaseTests.swift
+++ VKFeedTests/Feed Cache/CodableFeedStoreUseCaseTests.swift
@@ -9,16 +9,43 @@ import XCTest
 import VKFeed
 
 class CodableFeedStore {
+    private struct Cache: Codable {
+        var feed: [LocalFeedImage]
+        var timestamp: Date
+    }
+
+    private var cachedFeedPathURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        guard let cacheData = try? Data(contentsOf: cachedFeedPathURL) else {
+            completion(.empty)
+            return
+        }
+
+        let cache = try! JSONDecoder().decode(Cache.self, from: cacheData)
+        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let data = try! JSONEncoder().encode(Cache(feed: feed, timestamp: timestamp))
+        try! data.write(to: cachedFeedPathURL)
         completion(nil)
     }
 }
 
 class CodableFeedStoreUseCaseTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+        try? FileManager.default.removeItem(at: FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store"))
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+
+        try? FileManager.default.removeItem(at: FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store"))
+    }
+    
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
         
@@ -60,10 +87,10 @@ class CodableFeedStoreUseCaseTests: XCTestCase {
     func test_insert_deliversNoErrorOnEmptyCache() {
         let sut = makeSUT()
         let feed = makeUniqueImageFeed()
-        let currentDate = Date()
+        let fixedCurrentDate = Date()
         
         let expectation = expectation(description: "Wait for the completion to execute")
-        sut.insert(feed.local, timestamp: currentDate) { error in
+        sut.insert(feed.local, timestamp: fixedCurrentDate) { error in
             if let error = error {
                 XCTFail("Expected no errors, but received error instead \(error)")
             }
@@ -74,11 +101,38 @@ class CodableFeedStoreUseCaseTests: XCTestCase {
         wait(for: [expectation], timeout: 1.0)
     }
     
+    func test_retrieve_deliversDataOnNonEmptyCache() {
+        let sut = makeSUT()
+        let feed = makeUniqueImageFeed()
+        let fixedCurrentDate = Date()
+        
+        let expectation = expectation(description: "Wait for the completion to execute")
+        
+        sut.insert(feed.local, timestamp: fixedCurrentDate) { error in
+            XCTAssertNil(error, "Expected to insert feed without errors")
+            
+            sut.retrieve() { result in
+                switch result {
+                case let .found(receivedFeed, receivedTimestamp):
+                    XCTAssertEqual(receivedFeed, feed.local)
+                    XCTAssertEqual(receivedTimestamp, fixedCurrentDate)
+                default:
+                    XCTFail("Expected to found feed, but received \(result)")
+                }
+                
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    
     // MARK: - Helpers
     
-    private func makeSUT() -> CodableFeedStore {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
         let sut = CodableFeedStore()
-        trackForMemoryLeaks(sut)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
 }
