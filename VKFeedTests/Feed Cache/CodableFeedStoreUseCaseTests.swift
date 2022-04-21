a1227ef0b6bead68be88a24e3c8a659013d9397c
Vadim Khomenok <v.khomenok@gmail.com>
2022-04-21 17:34:02 +0200

    Insert data with empty cache delivers no error

--- VKFeedTests/Feed Cache/CodableFeedStoreUseCaseTests.swift
+++ VKFeedTests/Feed Cache/CodableFeedStoreUseCaseTests.swift
@@ -9,9 +9,12 @@ import XCTest
 import VKFeed
 
 class CodableFeedStore {
-    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         completion(.empty)
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        completion(nil)
     }
 }
 
@@ -49,6 +52,23 @@ class CodableFeedStoreUseCaseTests: XCTestCase {
                 
                 expectation.fulfill()
             }
+        }
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
+        let feed = makeUniqueImageFeed()
+        let currentDate = Date()
+        
+        let expectation = expectation(description: "Wait for the completion to execute")
+        sut.insert(feed.local, timestamp: currentDate) { error in
+            if let error = error {
+                XCTFail("Expected no errors, but received error instead \(error)")
+            }
+            
+            expectation.fulfill()
         }
         
         wait(for: [expectation], timeout: 1.0)
