d4952b8b406da217afb02c9c7a40f8a8334cbdf0
Vadim Khomenok <v.khomenok@gmail.com>
2022-04-24 23:59:07 +0200

    Added code documentation for FeedStore protocol that its operations can be invoked in any thread which means that clients are responsible to dispatch to appropriate threads if needed

--- VKFeed/Feed Cache/FeedStore.swift
+++ VKFeed/Feed Cache/FeedStore.swift
@@ -18,7 +18,15 @@ public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
     
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func deleteCache(_ completion: @escaping DeletionCompletion)
 }
