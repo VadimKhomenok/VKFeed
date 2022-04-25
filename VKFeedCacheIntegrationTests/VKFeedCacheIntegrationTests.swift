aa100cd7fe76f8ef6f6de74c56186a7ce8886ce1
Vadim Khomenok <v.khomenok@gmail.com>
2022-04-25 22:38:47 +0200

    Added new target for cache integration tests

---
+++ VKFeedCacheIntegrationTests/VKFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  VKFeedCacheIntegrationTests.swift
+//  VKFeedCacheIntegrationTests
+//
+//  Created by Vadim Khomenok on 25.04.22.
+//
+
+import XCTest
+
+class VKFeedCacheIntegrationTests: XCTestCase {
+
+}
