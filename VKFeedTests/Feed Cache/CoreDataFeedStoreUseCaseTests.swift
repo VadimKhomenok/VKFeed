41ef41d0805570fbbfc5cd555458e1e8d5dd9ae1
Vadim Khomenok <v.khomenok@gmail.com>
2022-04-25 13:55:44 +0200

    Added CoreDataFeedStore class which conforms to FeedStore protocol; CoreDataFeedStore retrieve delivers empty on empty cache

---
+++ VKFeedTests/Feed Cache/CoreDataFeedStoreUseCaseTests.swift
@@ -0,0 +1,74 @@
+//
+//  CoreDataFeedStoreUseCaseTests.swift
+//  VKFeedTests
+//
+//  Created by Vadim Khomenok on 25.04.22.
+//
+
+import XCTest
+import VKFeed
+
+class CoreDataFeedStore: FeedStore {
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+    func deleteCache(_ completion: @escaping DeletionCompletion) {
+        
+    }
+}
+
+class CoreDataFeedStoreUseCaseTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CoreDataFeedStore()
+        assertsThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_noSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversDataOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_noSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorWhenOverrideNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesNonEmptyCache() {
+        
+    }
+    
+    func test_delete_noErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_noSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_noErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptyCacheAfterDeleteCachedData() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+}
