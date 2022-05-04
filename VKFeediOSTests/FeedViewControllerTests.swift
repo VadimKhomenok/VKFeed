1c660c438f2d272e01f9dfc57c759e038ccb0f00
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-04 23:38:14 +0200

    Extracted the creation of SUT into helper factory method, added test for memory leaks

--- VKFeediOSTests/FeedViewControllerTests.swift
+++ VKFeediOSTests/FeedViewControllerTests.swift
@@ -28,15 +28,13 @@ final class FeedViewController: UIViewController {
 
 final class FeedViewControllerTests: XCTestCase {
     func test_init_doesNotLoadFeed() {
-        let loader = LoaderSpy()
-        _ = FeedViewController(loader: loader)
+        let (loader, _) = makeSUT()
         
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
     func test_onPresented_loadFeed() {
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
+        let (loader, sut) = makeSUT()
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(loader.loadCallCount, 1)
@@ -44,6 +42,16 @@ final class FeedViewControllerTests: XCTestCase {
     
     
     // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (loader: LoaderSpy, sut: FeedViewController) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (loader, sut)
+    }
 
     class LoaderSpy: FeedLoader {
         private(set) var loadCallCount: Int = 0
