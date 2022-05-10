b3632b5254e93b6453bcbd4c9227bdad6d935920
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-11 01:40:25 +0200

    FeedImageCellController moved to separate file and made 'final'

---
+++ VKFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -0,0 +1,56 @@
+//
+//  FeedImageCellController.swift
+//  VKFeediOS
+//
+//  Created by Vadim Khomenok on 11.05.22.
+//
+
+import VKFeed
+import UIKit
+
+final class FeedImageCellController {
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    private var task: FeedImageDataLoaderTask?
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func view() -> UITableViewCell {
+        let cell = FeedImageCell()
+        cell.descriptionLabel.text = model.description
+        cell.locationLabel.text = model.location
+        cell.locationContainer.isHidden = (model.location == nil)
+        cell.descriptionLabel.isHidden = (model.description == nil)
+        cell.feedImageView.image = nil
+        cell.retryButton.isHidden = true
+        cell.feedImageContainer.startShimmering()
+        
+        let loadImage = { [weak self, weak cell] in
+            guard let self = self else { return }
+            
+            self.task = self.imageLoader.loadImageData(from: self.model.url, completion: { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.retryButton.isHidden = (image != nil)
+                cell?.feedImageContainer.stopShimmering()
+            })
+        }
+        
+        cell.onRetry = loadImage
+        loadImage()
+
+        return cell
+    }
+    
+    func preload() {
+        task = imageLoader.loadImageData(from: model.url) { _ in }
+    }
+    
+    deinit {
+        task?.cancel()
+    }
+}
