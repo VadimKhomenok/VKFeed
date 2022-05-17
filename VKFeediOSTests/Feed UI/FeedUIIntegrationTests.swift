f4b66fc41d0c58f75468dff80626ed513e497d9b
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-18 01:38:25 +0200

    [REFACTORING] Fixed the title of the thread test (previous was mine)

--- VKFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ VKFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -269,7 +269,7 @@ final class FeedUIIntegrationTests: XCTestCase {
         XCTAssertNil(view?.renderedImage, "Expected no rendered image when image load finishes after the view is not visible anymore")
     }
 
-    func test_feedLoads_updatesUIInMainThread() {
+    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
         let (loader, sut) = makeSUT()
         
         sut.loadViewIfNeeded()
