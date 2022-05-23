65d2c448d855b02fe503a53a6eed1cb6a0dc651d
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-24 01:18:56 +0200

    FeedPresenterTests - use Set for messages for ViewSpy instead of array to not add restrictions for calls in didStartLoadingFeed as the order is not important there

--- VKFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ VKFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -73,19 +73,19 @@ class FeedPresenterTests: XCTestCase {
     
     private class ViewSpy: FeedLoadingView, FeedErrorView {
         
-        enum Messages: Equatable {
+        enum Messages: Hashable {
             case display(errorMessage: String?)
             case display(isLoading: Bool)
         }
         
-        private(set) var messages = [Messages]()
+        private(set) var messages = Set<Messages>()
         
         func display(_ viewModel: FeedLoadingViewModel) {
-            messages.append(.display(isLoading: viewModel.isLoading))
+            messages.insert(.display(isLoading: viewModel.isLoading))
         }
         
         func display(_ viewModel: FeedErrorViewModel) {
-            messages.append(.display(errorMessage: viewModel.message))
+            messages.insert(.display(errorMessage: viewModel.message))
         }
     }
 }
