46e56491f9cc673329cc545ef48dfef515e4f5b5
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-24 01:39:14 +0200

    FeedPresenter has localized title

--- VKFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ VKFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -45,6 +45,13 @@ final class FeedPresenter {
     private let loadingView: FeedLoadingView
     private let feedErrorView: FeedErrorView
     
+    static var title: String {
+        return NSLocalizedString("FEED_VIEW_TITLE",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "Title for the feed view")
+    }
+    
     var feedLoadError: String {
         return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
                                  tableName: "Feed",
@@ -75,6 +82,10 @@ final class FeedPresenter {
 }
 
 class FeedPresenterTests: XCTestCase {
+    
+    func test_presenter_hasLocalizedTitle() {
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
     
     func test_presenterLoad_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
