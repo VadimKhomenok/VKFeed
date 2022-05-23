4301d9b213d20f9187cfb8de906099f08a1ee7dd
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-23 23:19:18 +0200

    Show and Hide error message animated

--- VKFeediOS/Feed UI/Views/ErrorView.swift
+++ VKFeediOS/Feed UI/Views/ErrorView.swift
@@ -11,13 +11,41 @@ public final class ErrorView: UIView {
     @IBOutlet var label: UILabel!
     
     public var message: String? {
-        get { return label.text }
-        set { label.text = newValue }
+        get { return isVisible ? label.text : .none }
+        set { setMessageAnimated(newValue) }
+    }
+    
+    private var isVisible: Bool {
+        return self.alpha == 1
     }
     
     public override func awakeFromNib() {
         super.awakeFromNib()
         
         label.text = nil
+        alpha = 0
+    }
+    
+    private func setMessageAnimated(_ message: String?) {
+        if let message = message {
+            showAnimated(message)
+        } else {
+            hideMessageAnimated()
+        }
+    }
+    
+    private func showAnimated(_ message: String) {
+        label.text = message
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+    
+    private func hideMessageAnimated() {
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 0
+        } completion: { isFinished in
+            if isFinished { self.label.text = nil }
+        }
     }
 }
