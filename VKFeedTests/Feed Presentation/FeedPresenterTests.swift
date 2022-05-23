79b6ef79cb8a79aa314a8636b9326c7a2cd873af
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-24 00:19:06 +0200

    Extract SUT creation into test helper method

--- VKFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ VKFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -17,9 +17,20 @@ final class FeedPresenter {
 class FeedPresenterTests: XCTestCase {
     
     func test_presenterLoad_doesNotSendMessagesToView() {
-        let view = ViewSpy()
+        let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty, "Expected no messages to view on initialization")
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(view, file: file, line: line)
+        return (sut, view)
     }
     
     private class ViewSpy {
