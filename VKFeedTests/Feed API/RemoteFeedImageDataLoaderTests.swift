2258a058b3d19f83be9914b0106a8cc4cbc01e9f
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-26 00:23:10 +0200

    loadImageData requests image data from url (and twice - if called twice)

--- VKFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ VKFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -9,10 +9,16 @@ import VKFeed
 import XCTest
 
 class RemoteFeedImageDataLoader {
-    private var client: HTTPClient?
+    private var client: HTTPClient
     
     init(client: HTTPClient) {
-        
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL) {
+        client.get(from: url, completion: { _ in
+            
+        })
     }
 }
 
@@ -21,8 +27,22 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
     func test_init_doesNotRequestData() {
         let (_, client) = makeSUT()
         
-        XCTAssertTrue(client.messages.isEmpty)
+        XCTAssertTrue(client.messages.isEmpty, "Expected not to send any requests on initialization")
     }
+    
+    func test_loadImageData_requestsImageDataFromURL() {
+        let (sut, client) = makeSUT()
+        let url = anyURL()
+        
+        sut.loadImageData(from: url)
+        XCTAssertEqual(client.requestedURLs, [url], "Expected to request a URL when loadImageData called")
+        
+        sut.loadImageData(from: url)
+        XCTAssertEqual(client.requestedURLs, [url, url], "Expected to request twice when loadImageData called twice")
+    }
+
+    
+    // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, httpClient: HTTPClientSpy) {
         let httpClient = HTTPClientSpy()
