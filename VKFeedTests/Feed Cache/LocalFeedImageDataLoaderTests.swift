e5486a7bb24a5e876fd67dc740d9ccd768fca9eb
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-31 00:01:13 +0200

    Added helper method for LocalFeedImageDataLoaderTests; made LocalFeedImageDataLoader a final class

--- VKFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ VKFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -9,7 +9,7 @@ import Foundation
 import XCTest
 import VKFeed
 
-struct LocalFeedImageDataLoader {
+final class LocalFeedImageDataLoader {
     private let store: Any
     
     init(store: Any) {
@@ -23,6 +23,17 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         let _ = LocalFeedImageDataLoader(store: store)
         
         XCTAssertTrue(store.messages.isEmpty)
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
     }
     
     private class FeedStoreSpy {
