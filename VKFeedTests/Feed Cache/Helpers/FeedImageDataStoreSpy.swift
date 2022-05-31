d812c366c2003dd651e818dd94783f34431c6d6f
Vadim Khomenok <v.khomenok@gmail.com>
2022-05-31 21:48:40 +0200

    LoadFeedImageDataLoader save image data fails on store insertion error

--- VKFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ VKFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -17,14 +17,20 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
     
     private(set) var messages = [ReceivedMessages]()
     private(set) var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    private(set) var insertionCompletions = [(FeedImageDataStore.InsertionResult) -> Void]()
 
     func insert(_ imageData: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
         messages.append(.insert(data: imageData, for: url))
+        insertionCompletions.append(completion)
     }
     
     func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         messages.append(.retrieve(dataFor: url))
         retrievalCompletions.append(completion)
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
     }
     
     func completeRetrieval(with error: Error, at index: Int = 0) {
