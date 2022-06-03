33aa1c314d219a0caadb438e1a1b88fa462812a2
Vadim Khomenok <v.khomenok@gmail.com>
2022-06-03 02:32:19 +0200

    Test case for Composite Remote + Local Feed Loaders was renamed; some more renaming and minor changes

--- VKFeedApp/VKFeedAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ VKFeedApp/VKFeedAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -30,25 +30,25 @@ class FeedLoaderWithFallbackComposite {
     }
 }
 
-class RemoteLoaderWithLocalFallbackLoaderCompositeTests: XCTestCase {
+class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
     func test_loadFeed_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = makeUniqueFeed()
         let fallbackFeed = makeUniqueFeed()
-        let sut = makeSUT(primaryLoaderResult: .success(primaryFeed), fallbackLoaderResult: .success(fallbackFeed))
+        let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
         
         expect(sut, toDeliver: .success(primaryFeed))
     }
     
     func test_loadFeed_deliversFallbackFeedOnPrimaryFeedLoaderFailure() {
         let fallbackFeed = makeUniqueFeed()
-        let sut = makeSUT(primaryLoaderResult: .failure(anyNSError()), fallbackLoaderResult: .success(fallbackFeed))
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
         
         expect(sut, toDeliver: .success(fallbackFeed))
     }
     
     func test_loadFeed_deliversErrorOnPrimaryAndFallbackLoadersFailure() {
-        let sut = makeSUT(primaryLoaderResult: .failure(anyNSError()), fallbackLoaderResult: .failure(anyNSError()))
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .failure(anyNSError()))
         
         expect(sut, toDeliver: .failure(anyNSError()))
     }
@@ -56,13 +56,13 @@ class RemoteLoaderWithLocalFallbackLoaderCompositeTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT(primaryLoaderResult: FeedLoader.Result, fallbackLoaderResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line) -> FeedLoaderWithFallbackComposite {
-        let primaryLoaderStub = FeedLoaderStub(result: primaryLoaderResult)
-        let fallbackLoaderStub = FeedLoaderStub(result: fallbackLoaderResult)
-        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoaderStub, fallback: fallbackLoaderStub)
+    private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line) -> FeedLoaderWithFallbackComposite {
+        let primaryStub = FeedLoaderStub(result: primaryResult)
+        let fallbackStub = FeedLoaderStub(result: fallbackResult)
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryStub, fallback: fallbackStub)
         
-        trackForMemoryLeaks(primaryLoaderStub, file: file, line: line)
-        trackForMemoryLeaks(fallbackLoaderStub, file: file, line: line)
+        trackForMemoryLeaks(primaryStub, file: file, line: line)
+        trackForMemoryLeaks(fallbackStub, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         
         return sut
@@ -70,16 +70,17 @@ class RemoteLoaderWithLocalFallbackLoaderCompositeTests: XCTestCase {
     
     private func expect(_ sut: FeedLoaderWithFallbackComposite, toDeliver expectedResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "Wait for load to complete")
+        
         sut.load() { result in
             switch (result, expectedResult) {
             case let (.success(resultFeed), .success(expectedFeed)):
-                XCTAssertEqual(resultFeed, expectedFeed, "Expected to receive \(expectedFeed) feed, received \(resultFeed) instead")
+                XCTAssertEqual(resultFeed, expectedFeed, "Expected to receive \(expectedFeed) feed, received \(resultFeed) instead", file: file, line: line)
                 
             case let (.failure(error as NSError?), .failure(expectedError as NSError?)):
-                XCTAssertEqual(error, expectedError, "Expected to receive \(String(describing: expectedError)) error, received \(String(describing: error)) instead")
+                XCTAssertEqual(error, expectedError, "Expected to receive \(String(describing: expectedError)) error, received \(String(describing: error)) instead", file: file, line: line)
                 
             default:
-                XCTFail("Expected load to succeed")
+                XCTFail("Expected \(expectedResult), got \(result) instead", file: file, line: line)
             }
             
             exp.fulfill()
