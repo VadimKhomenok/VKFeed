07bfc8dc3a21e9062a926657e5787002f9d858bd
Vadim Khomenok <v.khomenok@gmail.com>
2022-06-03 02:01:27 +0200

    Composite remote Loader with local fallback loader - delivers local feed on remote feed loader load failure

--- VKFeedApp/VKFeedAppTests/RemoteLoaderWithLocalFallbackLoaderCompositeTests.swift
+++ VKFeedApp/VKFeedAppTests/RemoteLoaderWithLocalFallbackLoaderCompositeTests.swift
@@ -10,13 +10,23 @@ import VKFeed
 
 class RemoteLoaderWithLocalFallbackLoaderComposite {
     private let remoteFeedLoader: FeedLoader
+    private let localFeedLoader: FeedLoader
     
-    init(remoteFeedLoader: FeedLoader) {
+    init(remoteFeedLoader: FeedLoader, localFeedLoader: FeedLoader) {
         self.remoteFeedLoader = remoteFeedLoader
+        self.localFeedLoader = localFeedLoader
     }
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        remoteFeedLoader.load(completion: completion)
+        remoteFeedLoader.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+                
+            case .failure:
+                self?.localFeedLoader.load(completion: completion)
+            }
+        }
     }
 }
 
@@ -24,14 +34,39 @@ class RemoteLoaderWithLocalFallbackLoaderCompositeTests: XCTestCase {
     
     func test_loadFeed_deliversRemoteFeedOnRemoteSuccess() {
         let remoteFeed = makeUniqueFeed()
+        let localFeed = makeUniqueFeed()
         let remoteLoaderStub = RemoteFeedLoaderStub(result: .success(remoteFeed))
-        let sut = RemoteLoaderWithLocalFallbackLoaderComposite(remoteFeedLoader: remoteLoaderStub)
+        let localLoaderStub = LocalFeedLoaderStub(result: .success(localFeed))
+        let sut = RemoteLoaderWithLocalFallbackLoaderComposite(remoteFeedLoader: remoteLoaderStub, localFeedLoader: localLoaderStub)
         
         let exp = expectation(description: "Wait for load to complete")
         sut.load() { result in
             switch result {
             case let .success(resultFeed):
                 XCTAssertEqual(resultFeed, remoteFeed, "Expected to receive remote feed on Remote loader load success")
+                
+            case .failure:
+                XCTFail("Expected load to succeed")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_loadFeed_deliversLocalFeedOnRemoteFeedFailure() {
+        let remoteFeed = makeUniqueFeed()
+        let localFeed = makeUniqueFeed()
+        let remoteLoaderStub = RemoteFeedLoaderStub(result: .failure(anyNSError()))
+        let localLoaderStub = LocalFeedLoaderStub(result: .success(localFeed))
+        let sut = RemoteLoaderWithLocalFallbackLoaderComposite(remoteFeedLoader: remoteLoaderStub, localFeedLoader: localLoaderStub)
+        
+        let exp = expectation(description: "Wait for load to complete")
+        sut.load() { result in
+            switch result {
+            case let .success(resultFeed):
+                XCTAssertEqual(resultFeed, localFeed, "Expected to receive remote feed on Remote loader load success")
                 
             case .failure:
                 XCTFail("Expected load to succeed")
@@ -64,5 +99,21 @@ class RemoteLoaderWithLocalFallbackLoaderCompositeTests: XCTestCase {
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             completion(self.result)
         }
+    }
+    
+    private class LocalFeedLoaderStub: FeedLoader {
+        private let result: FeedLoader.Result
+        
+        init(result: FeedLoader.Result) {
+            self.result = result
+        }
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            completion(self.result)
+        }
+    }
+    
+    func anyNSError() -> NSError {
+        return NSError(domain: "An error", code: 400)
     }
 }
