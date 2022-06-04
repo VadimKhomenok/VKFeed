cc7d8c4c838330fd59bb33b302265cb8d2df4198
Vadim Khomenok <v.khomenok@gmail.com>
2022-06-04 23:09:37 +0200

    Add UI Test target for running high-level Acceptance Tests

---
+++ VKFeedApp/VKFeedAppUIAcceptanceTests/VKFeedAppUIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  VKFeedAppUIAcceptanceTests.swift
+//  VKFeedAppUIAcceptanceTests
+//
+//  Created by Vadim Khomenok on 4.06.22.
+//
+
+import XCTest
+
+class VKFeedAppUIAcceptanceTests: XCTestCase {
+
+}
