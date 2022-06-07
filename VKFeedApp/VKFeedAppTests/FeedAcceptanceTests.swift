1a4bbdb0a149da30a70476b4d47b99fb9442913f
Vadim Khomenok <v.khomenok@gmail.com>
2022-06-07 23:03:28 +0200

    Move HTTPClientStub to a shared scope in a separate file

--- VKFeedApp/VKFeedAppTests/FeedAcceptanceTests.swift
+++ VKFeedApp/VKFeedAppTests/FeedAcceptanceTests.swift
@@ -98,32 +98,6 @@ class FeedAcceptanceTests: XCTestCase {
     }
 }
 
-private class HTTPClientStub: HTTPClient {
-    
-    private class Task: HTTPClientTask {
-        func cancel() {}
-    }
-    
-    private let stub: (URL) -> HTTPClient.Result
-    
-    init(stub: @escaping (URL) -> HTTPClient.Result) {
-        self.stub = stub
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        completion(stub(url))
-        return Task()
-    }
-    
-    static var offline: HTTPClientStub {
-        HTTPClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
-    }
-    
-    static func online(_ stub: @escaping (URL) -> (Data, HTTPURLResponse)) -> HTTPClientStub {
-        HTTPClientStub { url in .success(stub(url)) }
-    }
-}
-
 private class InMemoryFeedStore: FeedStore, FeedImageDataStore {
     private var feedCache: CachedFeed?
     private var feedImageDataCache: [URL: Data] = [:]
