71a986a0566a76d05fdaa48d3838e0b15aa44085
Vadim Khomenok <v.khomenok@gmail.com>
2022-06-16 23:02:29 +0200

    Added ImageComment

---
+++ VKFeed/VKFeed/Feed API/ImageComment.swift
@@ -0,0 +1,15 @@
+//
+//  ImageComment.swift
+//  VKFeed
+//
+//  Created by Vadim Khomenok on 16.06.22.
+//
+
+import Foundation
+
+struct ImageComment {
+    var id: UUID
+    var message: String
+    var date: Date
+    var username: String
+}
