d0d8279dc41cb19affa1ea2e8f50d6ce46dd89b9
Vadim Khomenok <v.khomenok@gmail.com>
2022-06-17 22:39:34 +0200

    Replaced the implementation of RemoteFeedLoader with typealias to generic RemoteLoader and extension with convenience initializer to initialize RemoteLoader with the corresponding mapper for [Feedimage] resource type

--- VKFeed/VKFeed/Feed API/RemoteFeedLoader.swift
+++ VKFeed/VKFeed/Feed API/RemoteFeedLoader.swift
@@ -7,40 +7,10 @@
 
 import Foundation
 
-public class RemoteFeedLoader: FeedLoader {
-    private var client: HTTPClient
-    private var url: URL
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = FeedLoader.Result
-    
-    public init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            switch result {
-            case let .success((data, response)):
-                completion(Self.map(data: data, response: response))
-            case .failure(_):
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private static func map(data: Data, response: HTTPURLResponse) -> Result {
-        do {
-            let feedItems = try FeedItemsMapper.map(data, from: response)
-            return .success(feedItems)
-        } catch {
-            return .failure(error)
-        }
+public typealias RemoteFeedLoader = RemoteLoader<[FeedImage]>
+
+public extension RemoteFeedLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: FeedItemsMapper.map)
     }
 }
