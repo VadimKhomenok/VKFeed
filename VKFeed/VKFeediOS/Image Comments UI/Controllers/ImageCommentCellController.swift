e2f42238d2d9205fcf623efc48ec95426ac59580
Vadim Khomenok <v.khomenok@gmail.com>
2022-06-23 01:31:49 +0200

    Implement Image Comments UI

---
+++ VKFeed/VKFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,33 @@
+//
+//  ImageCommentCellController.swift
+//  VKFeediOS
+//
+//  Created by Vadim Khomenok on 23.06.22.
+//
+
+import UIKit
+import VKFeed
+
+public final class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+    
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+    
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ImageCommentCell.self)) as! ImageCommentCell
+        cell.messageLabel.text = model.message
+        cell.usernameLabel.text = model.username
+        cell.dateLabel.text = model.date
+        return cell
+    }
+    
+    public func preload() {
+        
+    }
+    
+    public func cancelLoad() {
+        
+    }
+}
---
+++ VKFeed/VKFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  VKFeediOS
+//
+//  Created by Vadim Khomenok on 23.06.22.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
