3993e533b9ce1b83dfc2a3325c25f478fb9d621c
Vadim Khomenok <v.khomenok@gmail.com>
2022-07-05 23:55:44 +0200

    Replaced record snapshots with assert snapshots for LoadMore tests; moved LoadMoreCell to a separate file

--- VKFeed/VKFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ VKFeed/VKFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -5,36 +5,8 @@
 //  Created by Vadim Khomenok on 5.07.22.
 //
 
-import Foundation
 import UIKit
 import VKFeed
-
-final class LoadMoreCell: UITableViewCell {
-    private lazy var spinner: UIActivityIndicatorView = {
-        let spinner = UIActivityIndicatorView(style: .medium)
-        contentView.addSubview(spinner)
-        
-        spinner.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40)
-        ])
-        
-        return spinner
-    }()
-    
-    public var isLoading: Bool {
-        get { spinner.isAnimating }
-        set {
-            if newValue {
-                spinner.startAnimating()
-            } else {
-                spinner.stopAnimating()
-            }
-        }
-    }
-}
 
 public final class LoadMoreCellController: NSObject, UITableViewDataSource {
     private let cell = LoadMoreCell()
