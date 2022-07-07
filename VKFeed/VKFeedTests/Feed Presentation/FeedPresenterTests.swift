173197f1ef51af34cc209f923aa5dd9b9921e2a5
Vadim Khomenok <v.khomenok@gmail.com>
2022-07-07 23:54:12 +0200

    Removed unused FeedViewModel and map function in FeedPresenter with test for it

--- VKFeed/VKFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ VKFeed/VKFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -14,14 +14,6 @@ class FeedPresenterTests: XCTestCase {
         XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
     }
     
-    func test_presenter_createsViewModel() {
-        let feed = [makeUniqueImage()]
-        
-        let viewModel = FeedPresenter.map(feed)
-        
-        XCTAssertEqual(viewModel.feed, feed)
-    }
-    
     // MARK: - Helpers
 
     func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
